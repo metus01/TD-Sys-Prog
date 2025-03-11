c284938a9093f98429244ccb3b69bbfd5a5adced
metus01 <carnelmetus@gmail.com>
2025-03-11 22:57:23 +0100

    add is_valide_base , and continue to_number function

--- TD3/exo3.c
+++ TD3/exo3.c
@@ -30,4 +30,17 @@ void free_number(number nbr)
 }
 
 
-number to_number (unsigned int nbr, unsigned char base)
+number to_number (unsigned int nbr, unsigned char gived_base)
+{
+    char *digits =  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+    if(is_valid_base(gived_base))
+    {
+        
+    }
+
+}
+
+int is_valid_base(base gived_base) {
+    return (gived_base == BASE_2 || gived_base == BASE_4 || gived_base == BASE_8 ||
+            gived_base == BASE_16 || gived_base == BASE_32 || gived_base == BASE_64);
+}
