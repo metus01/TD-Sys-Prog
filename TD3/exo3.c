850fd3f2c0a96f21b62dd0c52f71d97917e21ca0
metus01 <carnelmetus@gmail.com>
2025-03-12 06:26:13 +0100

    next step for to_number function

--- TD3/exo3.c
+++ TD3/exo3.c
@@ -33,12 +33,35 @@ void free_number(number nbr)
 number to_number (unsigned int nbr, unsigned char gived_base)
 {
     char *digits =  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+    char *result;
+    int index = 0;
     if(is_valid_base(gived_base))
     {
-        
+        // Gérer le cas spécial du nombre 0
+    if (nbr == 0) {
+        result[index++] = '0';
+        result[index] = '\0';
+        return;
     }
 
+    // Convertir en utilisant la division et les restes
+    while (nbr > 0) {
+        int remainder = nbr % gived_base;
+        result[index++] = digits[remainder];
+        nbr = nbr / gived_base;
+    }
+
+    // Inverser la chaîne de caractères
+    result[index] = '\0';
+    for (int i = 0, j = index - 1; i < j; i++, j--) {
+        char temp = result[i];
+        result[i] = result[j];
+        result[j] = temp;
+    }
 }
+
+    }
+
 
 int is_valid_base(base gived_base) {
     return (gived_base == BASE_2 || gived_base == BASE_4 || gived_base == BASE_8 ||
